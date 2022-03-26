f4ece21fea52b0d0669bcb3f12627ebf41c13048
Zen 0ne <gor.sharoyan95@gmail.com>
2022-03-26 13:42:18 +0400

    :green_heart:CI(components/Header&Footer):Created basic header/footer components,yet to be resized

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-//Routers
-
+//Router
 import SwitchRouter from "./routes/SwitchRouter";
+//Components
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <SwitchRouter />
+      <Footer />
     </div>
   );
 }
