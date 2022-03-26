0630dd8beb5fe79b7f090c9f477818faf205ad5c
Zen 0ne <gor.sharoyan95@gmail.com>
2022-03-26 12:35:55 +0400

    :green_heart:CI(components/CatCard):created separate component for every cat evaluation

---
+++ src/components/CatCard/CatCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function CatCard({ catName, catImage }) {
+  return;
+  <div>
+    <div>
+      <h4>{catName}</h4>
+    </div>
+    <div>
+      <img src={catImage} />
+    </div>
+  </div>;
+}
