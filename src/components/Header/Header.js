bf98e1ce5830dd79b8ff58ba70b869b2feccd747
Zen 0ne <gor.sharoyan95@gmail.com>
2022-03-26 14:35:32 +0400

    :art:UI(components/Header):worked more on Headers UI

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,8 +9,12 @@ import Logo from "../../Logo/Logo";
 export default function Header() {
   return (
     <header className="header">
-      <Logo />
-      Cats.API
+      <div className="headerComponent">
+        <Logo />
+      </div>
+      <div className="headerComponent">
+        <h3>Simple Web Page about Simple Cats</h3>
+      </div>
     </header>
   );
 }
